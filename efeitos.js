8d415e1779e3d8789803c75f52e292b8992e09ca
Gabriel Vieira <gabriel12.leo@gmail.com>
2024-11-13 10:26:13 -0300

    correção

--- efeitos.js
+++ efeitos.js
@@ -1,72 +1,79 @@
-(function ($) {
-    $(function () {
+document.addEventListener('DOMContentLoaded', function () {
   
+    // Declaração de variáveis
+    let agPosY = 0;
+    const agTimeline = document.querySelector('.js-timeline');
+    const agTimelineLine = document.querySelector('.js-timeline_line');
+    const agTimelineLineProgress = document.querySelector('.js-timeline_line-progress');
+    const agTimelinePoints = document.querySelectorAll('.js-timeline-card_point-box');
+    const agTimelineItems = document.querySelectorAll('.js-timeline_item');
+    const agOuterHeight = window.outerHeight;
+    let agHeight = window.innerHeight;
+    let previousPosY = -1;
+    let agFlag = false;
   
-      $(window).on('scroll', function () {
-        fnOnScroll();
+    // Evento de scroll para atualizar a timeline
+    window.addEventListener('scroll', fnOnScroll);
+  
+    // Evento de redimensionamento para atualizar a timeline
+    window.addEventListener('resize', fnOnResize);
+  
+    function fnOnScroll() {
+      agPosY = window.scrollY;
+      fnUpdateFrame();
+    }
+  
+    function fnOnResize() {
+      agPosY = window.scrollY;
+      agHeight = window.innerHeight;
+      fnUpdateFrame();
+    }
+  
+    function fnUpdateWindow() {
+      agFlag = false;
+  
+      // Atualização da linha de progresso da timeline
+      const firstPointTop = agTimelineItems[0].querySelector('.js-timeline-card_point-box').getBoundingClientRect().top + window.scrollY;
+      const firstItemTop = agTimelineItems[0].getBoundingClientRect().top + window.scrollY;
+      const lastPointTop = agTimelineItems[agTimelineItems.length - 1].querySelector('.js-timeline-card_point-box').getBoundingClientRect().top + window.scrollY;
+      const timelineTop = agTimeline.getBoundingClientRect().top + window.scrollY;
+      const timelineHeight = agTimeline.offsetHeight;
+  
+      agTimelineLine.style.top = (firstPointTop - firstItemTop) + "px";
+      agTimelineLine.style.bottom = (timelineTop + timelineHeight - lastPointTop) + "px";
+  
+      if (previousPosY !== agPosY) {
+        previousPosY = agPosY;
+        fnUpdateProgress();
+      }
+    }
+  
+    function fnUpdateProgress() {
+      const lastPointTop = agTimelineItems[agTimelineItems.length - 1].querySelector('.js-timeline-card_point-box').getBoundingClientRect().top + window.scrollY;
+      const progressTop = agTimelineLineProgress.getBoundingClientRect().top + window.scrollY;
+      let progressHeight = agPosY - progressTop + agOuterHeight / 2;
+  
+      if (lastPointTop <= agPosY + agOuterHeight / 2) {
+        progressHeight = lastPointTop - progressTop;
+      }
+      agTimelineLineProgress.style.height = progressHeight + "px";
+  
+      agTimelineItems.forEach(function (item) {
+        const itemTop = item.querySelector('.js-timeline-card_point-box').getBoundingClientRect().top + window.scrollY;
+  
+        if (itemTop < agPosY + 0.5 * agOuterHeight) {
+          item.classList.add('js-ag-active');
+        } else {
+          item.classList.remove('js-ag-active');
+        }
       });
+    }
   
-      $(window).on('resize', function () {
-        fnOnResize();
-      });
-  
-  
-      var agTimeline = $('.js-timeline'),
-        agTimelineLine = $('.js-timeline_line'),
-        agTimelineLineProgress = $('.js-timeline_line-progress'),
-        agTimelinePoint = $('.js-timeline-card_point-box'),
-        agTimelineItem = $('.js-timeline_item'),
-        agOuterHeight = $(window).outerHeight(),
-        agHeight = $(window).height(),
-        f = -1,
-        agFlag = false;
-  
-      function fnOnScroll() {
-        agPosY = $(window).scrollTop();
-  
-        fnUpdateFrame();
-      }
-  
-      function fnOnResize() {
-        agPosY = $(window).scrollTop();
-        agHeight = $(window).height();
-  
-        fnUpdateFrame();
-      }
-  
-      function fnUpdateWindow() {
-        agFlag = false;
-  
-        agTimelineLine.css({
-          top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
-          bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
-        });
-  
-        f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
-      }
-  
-      function fnUpdateProgress() {
-        var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
-  
-        i = agTop + agPosY - $(window).scrollTop();
-        a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
-        n = agPosY - a + agOuterHeight / 2;
-        i <= agPosY + agOuterHeight / 2 && (n = i - a);
-        agTimelineLineProgress.css({height: n + "px"});
-  
-        agTimelineItem.each(function () {
-          var agTop = $(this).find(agTimelinePoint).offset().top;
-  
-          (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
-        })
-      }
-  
-      function fnUpdateFrame() {
-        agFlag || requestAnimationFrame(fnUpdateWindow);
+    function fnUpdateFrame() {
+      if (!agFlag) {
+        requestAnimationFrame(fnUpdateWindow);
         agFlag = true;
       }
-  
-  
-    });
-  })(jQuery);
-  
+    }
+  
+  });
