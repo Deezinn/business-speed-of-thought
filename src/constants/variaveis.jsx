2b5908824a50a5a1b2c1a8fc0e9b6fb67c1ec320
deezinn <developer.andrelz@gmail.com>
2025-08-16 01:16:22 -0300

    feat: new component -> navbar
    Co-authored-by: Gabriel Freire de Barros Vieira <GabeFBVieira@users.noreply.github.com>

---
+++ src/constants/variaveis.jsx
@@ -0,0 +1,8 @@
+import { ImHome, ImVideoCamera, ImEyePlus } from "react-icons/im";
+
+
+export const navbarOptions = [
+  {"icone": <ImHome/>, "texto": "Home"},
+  {"icone": <ImVideoCamera/>, "texto": "Videos"},
+  {"icone": <ImEyePlus/>, "texto": "Insights"}
+]
