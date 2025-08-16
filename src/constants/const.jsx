46e41900bca1303831a5412b90b6d997a9c2e0a5
André Luiz <developer.andrelz@gmail.com>
2025-08-16 19:47:42 -0300

    Nextjs page (#6)
    * feat: new component -> navbar
    
    Co-authored-by: Gabriel Freire de Barros Vieira <GabeFBVieira@users.noreply.github.com>
    
    * feat: new style to main page
    
    Co-authored-by: Gabriel Freire de Barros Vieira <GabeFBVieira@users.noreply.github.com>
    
    * feat: new style to search
    
    * style(main): new style to main page
    
    ---------
    
    Signed-off-by: André Luiz <developer.andrelz@gmail.com>
    Co-authored-by: Gabriel Freire de Barros Vieira <GabeFBVieira@users.noreply.github.com>

--- src/constants/const.jsx
+++ src/constants/const.jsx
@@ -1,5 +1,7 @@
 import { ImHome, ImVideoCamera, ImEyePlus } from "react-icons/im";
 
+import { ImBubble2, ImEye, ImShare2 } from "react-icons/im";
+
 import bill_inovates from "@/assets/bill_inovates.webp"
 import bill_mocks_ai from "@/assets/bill_mocks_ai.webp"
 import interview_bill from "@/assets/interview_with_bill.webp"
@@ -15,6 +17,14 @@ export const optionsCategory = [
   'Finance', 'Health Care', 'Technology', 'Jobs', 'kids'
 ]
 
-export const billImages = [
-  bill_inovates, bill_mocks_ai, interview_bill
+export const billGatesContext = [
+  {"image": bill_inovates, "tag": "teste", "text": "lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul ", "icon": <ImEye />, "icon-label": "2.5M"},
+  {"image": bill_mocks_ai, "tag": "teste", "text": "lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul ", "icon": <ImEye />, "icon-label": "2.3m"},
+  {"image": interview_bill, "tag": "teste", "text": "lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul lorem ipsul ", "icon": <ImEye />, "icon-label": "2.5m"}
 ]
+
+export const iconOptions = [
+  {"icon": <ImEye />, "text": "2.5M"},
+  {"icon": <ImBubble2 />, "text": "2.15K"},
+  {"icon": <ImShare2 />, "text": "15K"},
+
