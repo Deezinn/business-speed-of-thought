463e30aeafc813aec554e6d238bf009928fcc4f2
deezinn <developer.andrelz@gmail.com>
2025-08-17 01:30:01 -0300

    style: new navbar style

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,39 @@
-import { navbarOptions } from '@/constants/const';
+import { navbarOptions } from "@/constants/const";
 import { ImSearch } from "react-icons/im";
 
 export default function Navbar() {
   return (
-    <>
-      <div className="flex flex-row w-full h-16 justify-center fixed bg-white z-10">
-        <div className="flex justify-center items-center text-center w-1/2">
+    <div className="flex flex-row w-full h-16 justify-center fixed bg-white z-10">
 
-          <h1 className="uppercase font-bold oswald-500 text-xl">Bill Gates</h1>
+      {/* Logo / Nome */}
+      <div className="flex justify-center items-center text-center w-1/2">
+        <h1 className="uppercase font-bold oswald-500 text-xl">Bill Gates</h1>
+      </div>
 
-        </div>
-        <div className="flex justify-evenly items-center text-center w-full" >
-          {
-            navbarOptions.map((opcao, idx) => (
-              <div className='flex flex-row cursor-pointer justify-center items-center text-center gap-2' key={idx}>
-                <div>{opcao.icone}</div>
-
-                <h1 className='oswald-300 text-xl'>{opcao.texto}</h1>
-
-              </div>
-            ))
-          }
-        </div>
-        <div className="flex justify-center items-center text-center w-1/2">
-          <div className='flex flex-row bg-gray-100 justify-center items-center text-center rounded-2xl p-1'>
-
-            <input type="text" className='w-3/4 focus:outline-none text-xl' placeholder='Pesquise' />
-
-
-            <ImSearch className='text-gray-500' />
+      {/* Opções do menu */}
+      <div className="flex justify-evenly items-center text-center w-full">
+        {navbarOptions.map((opcao, idx) => (
+          <div
+            key={idx}
+            className="flex flex-row cursor-pointer justify-center items-center text-center gap-2"
+          >
+            <div>{opcao.icone}</div>
+            <h1 className="oswald-300 text-xl">{opcao.texto}</h1>
           </div>
+        ))}
+      </div>
+
+      {/* Campo de busca */}
+      <div className="flex justify-center items-center text-center w-1/2">
+        <div className="flex flex-row bg-gray-100 justify-center items-center text-center rounded-2xl p-1">
+          <input
+            type="text"
+            className="w-3/4 focus:outline-none text-xl bg-transparent"
+            placeholder="Pesquise"
+          />
+          <ImSearch className="text-gray-500" />
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
